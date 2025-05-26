ff9ac18f9173d8d86871723bc984e712d9f8b494
student-dv <dvbeast465@gmail.com>
2025-05-27 00:56:29 +0530

    update Frontend

--- context/CartContext.js
+++ context/CartContext.js
@@ -1,5 +1,5 @@
 "use client";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { createContext, useContext, useState, useEffect } from "react";
 
 const CartContext = createContext();
